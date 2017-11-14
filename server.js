a692ac3ed24c4ff32ed04fe49c7ea0ca921ac97b
daoyi7 <kawhichina@gmail.com>
2017-11-14 20:57:03 +0800

    Hello World!

--- server.js
+++ server.js
@@ -4,33 +4,31 @@ const fs = require('fs')
 
 const dev = 'D:/Project/iframe/n'
 
-http.createServer((request, response) => {
+http.createServer((req, res) => {
 
   let file = dev + '/src/index.html';
 
   fs.readFile(file, (err, data) => {
     if(err) {
-      response.writeHeader(
+      res.writeHeader(
         404,
         {
-          // 'Content-Type': 'text/html:charset = "UTF-8"'
           'Content-Type' : 'text/html;charset="UTF-8"'
         }
       )
 
-      response.write('<h1>404</h1><p>你要找的页面不存在</p>')
-      response.end()
+      res.write('<h1>404</h1><p>你要找的页面不存在</p>')
+      res.end()
     }else {
-      response.writeHeader(
+      res.writeHeader(
         200,
         {
-          // 'Content-Type': 'text/html:charset = "UTF-8"'
           'Content-Type' : 'text/html;charset="UTF-8"'
         }
       )
 
-      response.write(data)
-      response.end()
+      res.write(data)
+      res.end()
     }
   })
 }).listen(PORT)

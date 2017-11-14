7cebdba7b5053294fc46404cc60c2e4b886e3129
daoyi7 <kawhichina@gmail.com>
2017-11-14 20:56:15 +0800

    Hello World

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
+const PORT = 2017
 const http = require('http')
+const fs = require('fs')
+
+const dev = 'D:/Project/iframe/n'
 
 http.createServer((request, response) => {
-  response.writeHead(
-    200,
-    {
-      'Content-Type': 'text/plain'
-    }
-  )
 
-  response.end('Hello World\n')
-}).listen(2017)
+  let file = dev + '/src/index.html';
+
+  fs.readFile(file, (err, data) => {
+    if(err) {
+      response.writeHeader(
+        404,
+        {
+          // 'Content-Type': 'text/html:charset = "UTF-8"'
+          'Content-Type' : 'text/html;charset="UTF-8"'
+        }
+      )
+
+      response.write('<h1>404</h1><p>你要找的页面不存在</p>')
+      response.end()
+    }else {
+      response.writeHeader(
+        200,
+        {
+          // 'Content-Type': 'text/html:charset = "UTF-8"'
+          'Content-Type' : 'text/html;charset="UTF-8"'
+        }
+      )
+
+      response.write(data)
+      response.end()
+    }
+  })
+}).listen(PORT)
 
 console.log('Server running at http://172.0.0.1:2017')

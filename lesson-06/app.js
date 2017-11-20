f39f543f44d54cd12bb0b59c64cf8f4e8e0dbec9
daoyi7 <kawhichina@gmail.com>
2017-11-20 14:17:52 +0800

    bug

--- lesson-06/app.js
+++ lesson-06/app.js
@@ -22,7 +22,7 @@ app.get('/', (req, res, next) => {
       let topicUrls = []
       let $ = cheerio.load(sres.text)
 
-      $('.nba-teamForum dd').find("a").each((index, ele) => {
+      $('.nba-teamForum dd').find("a").slice(0, 4).each((index, ele) => {
         const $ele = $(ele)
 
         let href = url.resolve(hcUrl, $ele.attr("href"))
@@ -33,20 +33,47 @@ app.get('/', (req, res, next) => {
       let concurrencyCount = 0
 
       const fetchUrl = (link, callback) => {
-        let delay = parseInt((Math.random() * 10000000) % 2000, 10)
 
+        let delay = parseInt((Math.random() * 10000000) % 2000, 10)
         concurrencyCount++
+        console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', link, '，耗时' + delay + '毫秒')
 
         setTimeout(() => {
           concurrencyCount--
+
           callback(null, link)
         }, delay)
       }
 
-      async.mapLimit(topicUrls, 5, (link, callback) => {
+      async.mapLimit(topicUrls, 2, (link, callback) => {
         fetchUrl(link, callback)
       }, (err, result) => {
         console.log('over')
+        res.send(result)
+        let aa = result.map((topicPair) => {
+          let $ = cheerio.load(topicPair[1])
+
+          console.log(topicPair[1])
+          return ({
+            href: topicPair,
+            title: $('#j_data').attr("data-title"),
+            first_comment: $('#readfloor td').eq(0).text().trim()
+          })
+        })
+        console.log(aa)
+        // result.map((t) => {
+        //   superagent.get(t)
+        //     .end((err, sres) => {
+        //       let $ = cheerio.load(sres.text)
+        //       let ta = []
+        //
+        //       ta.push({
+        //         href: t,
+        //         title: $('#j_data').attr("data-title")
+        //       })
+        //       console.log(ta)
+        //     })
+        // })
       })
     })
 })

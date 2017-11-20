ca93ac7570b9637cf3df8a4a4b4c89eb98145658
daoyi7 <kawhichina@gmail.com>
2017-11-20 10:07:38 +0800

    终于把 lesson-04 send到页面上了

--- lesson-04/app.js
+++ lesson-04/app.js
@@ -9,23 +9,21 @@ const hcUrl = 'https://nba.hupu.com/';
 
 app.get('/', (req, res, next) => {
   superagent.get(hcUrl)
-    .end((err, res) => {
+    .end((err, sres) => {
       if(err) {
         return console.error(err)
       }
 
       let topicUrls = []
-      let $ = cheerio.load(res.text)
+      let $ = cheerio.load(sres.text)
 
-      $('.nba-teamForum dd').slice(0, 4).find("a").each((index, ele) => {
+      $('.nba-teamForum dd').find("a").each((index, ele) => {
         const $ele = $(ele)
 
         let href = url.resolve(hcUrl, $ele.attr("href"))
 
         topicUrls.push(href)
       })
-
-      console.log(topicUrls)
 
       let ep = new eventproxy()
 
@@ -37,11 +35,11 @@ app.get('/', (req, res, next) => {
           return ({
             href: topicUrl,
             title: $('#j_data').attr("data-title"),
-            comment1: $('#readfloor td').eq(0).text().trim()
+            first_comment: $('#readfloor td').eq(0).text().trim()
           })
         })
 
-        console.log(topics)
+        res.send(topics)
       })
 
       topicUrls.forEach((topicUrl) => {

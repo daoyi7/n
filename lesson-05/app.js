0d829e08fa31f6aae03e6fc90f2d85c6ff487355
daoyi7 <kawhichina@gmail.com>
2017-11-22 22:38:27 +0800

    send lesson-05

--- lesson-05/app.js
+++ lesson-05/app.js
@@ -1,27 +1,78 @@
 const async = require('async')
+const express = require('express')
+const superagent = require('superagent')
+const cheerio = require('cheerio')
+const eventproxy = require('eventproxy');
+const url = require('url')
+const hc = 'https://voice.hupu.com/nba'
 
-let concurrencyCount = 0
+const app = express()
+app.listen(7777, () => {
+  console.log(7777)
+})
 
-const fetchUrl = (url, callback) => {
-  let delay = parseInt((Math.random() * 10000000) % 2000, 10)
+app.get('/', (req, res, next) => {
+  superagent.get(hc)
+    .end((err, sres) => {
+      if (err) {
+        console.error(err)
+      }
 
-  concurrencyCount++
-  console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', url, '，耗时' + delay + '毫秒')
+      const $ = cheerio.load(sres.text)
+      let urls = []
 
-  setTimeout(() => {
-    concurrencyCount--
-    callback(null, url + 'html content')
-  }, delay)
-}
+      $(".news-list li").each((idx, ele) => {
+        const $ele = $(ele)
 
-let urls = []
-for (let i = 0; i < 10; i++) {
-  urls.push('http://datasource_' + i)
-}
+        if (typeof($ele.attr("class")) == "undefined") {
+          const href = $ele.find("h4 a").attr("href")
 
-async.mapLimit(urls, 5, (url, callback) => {
-  fetchUrl(url, callback)
-}, (err, result) => {
-  console.log('final:')
-  console.log(result)
+          urls.push(href)
+        }
+      })
+
+      let concurrencyCount = 0
+
+      const fetchUrl = (url, callback) => {
+        let delay = parseInt((Math.random() * 10000000) % 2000, 10)
+
+        concurrencyCount++
+
+        console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', url, '，耗时' + delay + '毫秒')
+
+        setTimeout(() => {
+
+          concurrencyCount--
+
+          superagent.get(url)
+            .end((err, sres) => {
+              if(err) {
+                console.error("我要看这个："+err)
+              }
+
+              let $ = cheerio.load(sres.text)
+
+              callback (null, {
+                href: url,
+                id: url.split("").slice(27,34).join(""),
+                title: $('.headline').text().trim(),
+                upTime: $(".artical-info a.time span").eq(0).text().trim(),
+                source: $(".comeFrom a").eq(0).text().trim(),
+                thumb: $('.artical-importantPic img').eq(0).attr("src"),
+                info: $('.artical-main-content p').eq(0).text().trim()
+              })
+            })
+
+          // callback(null,url)
+        }, delay)
+      }
+
+      async.mapLimit(urls, 5, (url, callback) => {
+        fetchUrl(url, callback)
+      }, (err, result) => {
+        console.log('final:')
+        console.log(result)
+        res.send(result)
+      })
+  })
 })
